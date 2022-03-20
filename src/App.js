86ac418f960e3bd0d75c5704d240770214818e0d
khadiza21 <bibikhadiza474@gmail.com>
2022-03-21 02:51:57 +0600

    recap react 6 concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
     </div>
   );
 }
 
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  return (
+    <div>
+      <h1>Rest Countries</h1>
+      <h3>Available countries: {countries.length}</h3>
+      {countries.map((country) => (
+        <Country name={country.name} population={country.population}></Country>
+      ))}
+    </div>
+  );
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h4>{props.name}</h4>
+      <h5>{props.population}</h5>
+    </div>
+  );
+}
 export default App;
